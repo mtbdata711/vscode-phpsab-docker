67d910b04855f9acbad5e512a9b96304fbc4c1cc
Matthew Benge <m.t.benge@arts.ac.uk>
2024-01-29 03:40:34 +0000

    Convert all paths to posix

--- src/resolvers/docker-path-resolver.ts
+++ src/resolvers/docker-path-resolver.ts
@@ -9,35 +9,32 @@ export class DockerPathResolver {
     private filePath: string;
     private config: ResourceSettings;
     private logger: Logger;
-    private isWindows: boolean;
 
     constructor(filePath: string, config: ResourceSettings, logger: Logger) {
         this.filePath = filePath;
         this.config = config;
         this.logger = logger;
-        this.isWindows = /^win/.test(process.platform);
     }
 
-    // resolve docker path to local path from document.uri.fsPath
     public resolveDocker() {
         const { dockerWorkspaceRoot, workspaceRoot } = this.config;
         let { filePath } = this;
-        let normalisedWorkspaceRoot = workspaceRoot;
-        if(this.isWindows) {
-            normalisedWorkspaceRoot = workspaceRoot.replace(/\\/g, "/").toLowerCase();
-            filePath = filePath.replace(/\\/g, "/").toLowerCase();
-        }
-        const dockerPath = filePath.replace(normalisedWorkspaceRoot, dockerWorkspaceRoot);
+        const normalisedWorkspaceRoot = DockerPathResolver.toPosixPath(workspaceRoot);
+        const normalisedFilePath = DockerPathResolver.toPosixPath(filePath);
+        const dockerPath = normalisedFilePath.replace(normalisedWorkspaceRoot, dockerWorkspaceRoot);
         this.logger.logInfo(`DOCKER: Resolved docker path: ${dockerPath}`);
         return dockerPath;
     }
 
-    // resolve local path to docker path from document.uri.fsPath
     public resolveLocal() {
         const { dockerWorkspaceRoot, workspaceRoot } = this.config;
         const { filePath } = this;
         const localPath = filePath.replace(dockerWorkspaceRoot, workspaceRoot);
         this.logger.logInfo(`DOCKER: Resolved local path: ${localPath}`);
         return localPath;
+    }
+
+    static toPosixPath(path: string) {
+        return path.replace(/\\/g, "/");
     }
 }
